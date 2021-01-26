43c71cced7bcf39949c0b70d94a3b004ab9fdcee
Andres Fernando <andres.fernando08@gmail.com>
2021-01-25 22:04:30 -0500

    mapear datos de api con la pagina home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
-const Home = () => {
+import getData from '../utils/getData';
+
+const Home = async () => {
+  const characters = await getData();
   const view = `
     <div class="Characters">
-      <article class="Character-item">
-        <a href="#/1/">
-          <img src="image" alt="name" />
-          <h2>Name</h2>
-        </a>
-      </article>
+      ${characters.results.map(char => `
+        <article class="Character-item">
+          <a href="#/${char.id}/">
+            <img src="${char.image}" alt="${char.name}" />
+            <h2>${char.name}</h2>
+          </a>
+        </article>
+      `).join('')}
     </div>
   `;
   return view;
